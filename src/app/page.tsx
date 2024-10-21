8e3d7d5f5effb393bf3651ffbbcc0e70d671c23d
positivelyjon <positivelyjon@gmail.com>
2024-10-22 02:17:59 +0800

    feat: create sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navigation from "@/components/Navigation";
+
 export default function App() {
   return (
     <div className="w-full h-screen flex flex-row relative">
+      <Navigation />
       <div className="flex flex-col p-10 ml-20 w-full gap-5">
         <h1 className="text-4xl text-neutral-200">Dashboard</h1>
         <div className="w-full h-80 border border-neutral-500/50 bg-neutral-800/20 rounded"></div>
